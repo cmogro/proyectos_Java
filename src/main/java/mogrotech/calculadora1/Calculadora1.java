28e999f026713848ff8b8ce8b2937a15dc3e06ca
carlos <carlos@halo>
2025-03-18 19:09:19 -0300

    Comenzando mi project Calculadora

---
+++ src/main/java/mogrotech/calculadora1/Calculadora1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mogrotech.calculadora1;
+
+/**
+ *
+ * @author carlos
+ */
+public class Calculadora1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
